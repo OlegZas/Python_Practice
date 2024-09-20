5255d1037277ae621f66690232c5864ac67d9c6b
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-09-19 20:33:21 -0700

    Update Practice1.py

--- Practice1.py
+++ Practice1.py
@@ -70,3 +70,84 @@ x = bytes(8)	----- bytes
 x = bytearray(8)	----- bytearray	
 x = memoryview(bytes(8))	----- memoryview
 """
+# 9/19/2024 
+# Float - contains decimals and can have scientific e
+m = 54.45
+print(type(m))
+# Complex - numbers are written with a j as imaginary part 
+u =1j
+print(u)
+# ****type conversion ****
+k = int(m)
+p=1
+o = str(m)
+print(k, float(p), type(o))
+# for random numbers use 
+# import random 
+print(random.randrange(1,689))
+# ****************************
+# ************************************ STRING ***************
+# ************ INDEX OF ELEMENTS OF THE STRING : 
+e = 'Oleg'
+print(e[3])
+# *********** LOOPING THROUGH A STRING: 
+for x in 'olegario':
+    print(x)
+# ********* STIRNG LENGTH
+a = 'Olegor'
+print(len(a))
+# ***** check if a character or a string is present in a stirng:
+print('O' in a) # return boolean TRUE/False 
+if "O" in a :
+    print("yes, O is in", a)
+# **** Check if NOT
+print("O" not in a) # TRUE / FALSE 
+if "M" not in a : 
+    print(a)
+# SLICING - return a range of characters from x to y from a string
+print(a[2:4])
+# SLICING from the begging 
+print (a[:3])
+# SLICE from x to the end 
+print (a[1:])
+# SCLIDE FROM END OF THE STRING -- negative indexing 
+print (a[-3:-1]) # Olegor = go .. indexing starts from 1 
+# ****** MODIFYING STRING 
+print(a.upper())
+print(a.lower())
+# Remove whitespace (space before or after the string)
+# strip ()
+a = ' Oleg, Best ' 
+print(a.strip())
+# Replace() - replaces a string with antoher 
+print(a.replace('Oleg', 'Jack'))
+# split() - returns a list wehre the text between the separator is the list of items 
+print(a.split(','))
+#Concatenation
+b = " won"
+c = a+b
+print(a + b )
+print(c)
+# ** Combining STRINGS and NUMBERS 
+# cant simply do STRING + INT .. will result in error 
+age =  22
+g = f"""oleg 
+{age:.2f}  
+best"""
+print(g)# :.2f is a modifier reformatting the vlaue to 2 decimals 
+# **** ESCAPE CHARACTERS 
+# inserting illegal characters for ex: double quote witin a string 
+a = "Oleg is \"Olegor\" of the world"
+print(a) 
+"""
+Code	Result	
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+"""
